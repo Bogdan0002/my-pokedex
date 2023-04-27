afd953a8d3bf63a33db1dd36be5e75cc0435aa61
Marin Bilba <mabi@kamstrup.com>
2023-04-27 22:13:04 +0200

    Changed homepage link

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import { BrowserRouter } from 'react-router-dom';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PokemonDetails from './components/PokemonDetails';
 
 const router = createBrowserRouter([
   {
